201a60e5a95af08c41185bc46d70810eac6eb246
Adebiyi David <adebiyi137@gmail.com>
2019-08-21 20:40:05 +0100

    first commit

---
+++ src/components/search/Search.js
@@ -0,0 +1,57 @@
+import React from "react"
+import TextField from "material-ui/TextField"
+import SelectField from "material-ui/SelectField"
+import MenuItem from "material-ui/MenuItem"
+import axios from "axios"
+
+
+class SearchComponent extends React.Component{
+
+    constructor(){
+        super()
+        this.state={
+            searchText : "",
+            amount : 10,
+            apiUrl : "https://pixabay.com/api",
+            apiKey : "",
+            images : []
+        }
+        this.onTextChange = this.onTextChange.bind(this)
+    }
+    
+    onTextChange = (e) => {
+
+        const {name, value} = e.target
+        this.setState({[name] : value})
+    }
+
+    onAmount
+    render(){
+        return(
+            <div>
+                <TextField
+                    name="searchText"
+                    value={this.state.searchText}
+                    onChange={this.onTextChange}
+                    floatingLabelText="Search For Images"
+                    fullWidth={true}
+                />
+                <br/>
+                <SelectField
+                    name="amount"
+                    floatingLabelText="Amount"
+                    value={this.state.amount}
+                    onChange={this.onAmountChange}
+                >
+                    <MenuItem value={5} primaryText="5"/>
+                    <MenuItem value={10} primaryText="10"/>
+                    <MenuItem value={15} primaryText="15"/>
+                    <MenuItem value={30} primaryText="30"/>
+                    <MenuItem value={50} primaryText="50"/>
+                </SelectField>
+            </div>
+        )
+    }
+}
+
+export default SearchComponent
